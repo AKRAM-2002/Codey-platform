cd89c051f2db39bb5066171e2d861e204bea8e51
AKRAM-2002 <Akrambusiness6@gmail.com>
2025-01-30 12:50:42 +0100

    update

--- test.py
+++ test.py
@@ -1,23 +1,15 @@
-def make_change(amount):
+tâches = [
+    {"nom": "Tâche 1", "priorité": "élevée", "durée": 30},
+    {"nom": "Tâche 2", "priorité": "moyenne", "durée": 20},
+    {"nom": "Tâche 3", "priorité": "élevée", "durée": 10},
+]
 
-    denominations = [500, 100, 25, 10, 5, 1]
+def trier_tâches(tâches):
+    if len(tâches) > 50:
+        raise ValueError("Le nombre de tâches ne peut pas dépasser 50.")
+    priorité_ordre = {"élevée": 1, "moyenne": 2, "faible": 3}
+    return sorted(tâches, key=lambda x: (priorité_ordre[x["priorité"]], x["durée"]))
 
-    
-
-    coin_count = 0
-
-    
-
-    for coin in denominations:
-
-        coin_count += amount // coin
-        print(coin_count)
-
-        amount %= coin
-        print(amount)
-
-        
-
-    return coin_count
-
-print(make_change(468))
+tâches_triées = trier_tâches(tâches)
+for tâche in tâches_triées:
+    print(tâche)
